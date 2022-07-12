c945ac0ff64e1aa3ada85efe0055a0079cca9905
DIBYO-DEY <63423632+DIBO237@users.noreply.github.com>
2022-07-12 15:02:34 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 
 const { Sequelize } = require('sequelize');
-
+const db_name = "test"
+const username = "root"
+const password = ""
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('test', 'root', '', {
+const sequelize = new Sequelize(db_name, username, password, {
     dialect: 'mysql',
     dialectOptions: {
         host:"localhost"
